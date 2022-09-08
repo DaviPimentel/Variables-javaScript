54c18a1bb268881e6dff3e0bc52f1a729fb02fc1
davi pimentel <davipimentel9@gmail.com>
2022-09-08 17:05:41 -0300

    Exercício - Variables  javaScript

---
+++ javascript-3.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')();
+
+const weight = prompt('Type your weight: ');
+console.log(`The Typed  was: ${weight}`)
+
+const username = prompt('Type your name: ');
+console.log(`The Typed name was: ${username}`)
+
+const height = prompt('Type your height: ');
+console.log(`The Typed name was: ${height}`)
+
+const hospitalization = prompt('Type your date hospitalization: ');
+console.log(`The Typed name was: ${hospitalization}`)
+
+const diabetes = prompt('Type if you have diabetes, if yes type Y/if no type N: ');
+console.log(`The Typed name was: ${diabetes}`)
